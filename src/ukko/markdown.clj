fcaebeaee6ff0bf98ede2467450f3bf95b44a67a
Alain M. Lafon <alain@200ok.ch>
2022-03-08 16:32:28 +0100

    feat: Upgrade flexmark and improve MD rendering

--- src/ukko/markdown.clj
+++ src/ukko/markdown.clj
@@ -1,55 +1,29 @@
-;; Based upon https://github.com/hashobject/perun/blob/master/src/io/perun/markdown.clj
 (ns ukko.markdown
   (:require [clojure.java.io :as io])
-  (:import [com.vladsch.flexmark Extension]
-           [com.vladsch.flexmark.html HtmlRenderer]
+  (:import [com.vladsch.flexmark.html HtmlRenderer]
+           [com.vladsch.flexmark.ext.tables TablesExtension]
+           [com.vladsch.flexmark.ext.gfm.strikethrough StrikethroughExtension]
+           [com.vladsch.flexmark.ext.autolink AutolinkExtension]
+           [com.vladsch.flexmark.util.data MutableDataSet]
            [com.vladsch.flexmark.parser Parser]
-           [com.vladsch.flexmark.profiles.pegdown Extensions PegdownOptionsAdapter]))
+           [java.util Arrays]))
 
-(def extensions
-  {:smarts               Extensions/SMARTS
-   :quotes               Extensions/QUOTES
-   :smartypants          Extensions/SMARTYPANTS
-   :abbreviations        Extensions/ABBREVIATIONS
-   :hardwraps            Extensions/HARDWRAPS
-   :autolinks            Extensions/AUTOLINKS
-   :tables               Extensions/TABLES
-   :definitions          Extensions/DEFINITIONS
-   :fenced-code-blocks   Extensions/FENCED_CODE_BLOCKS
-   :wikilinks            Extensions/WIKILINKS
-   :strikethrough        Extensions/STRIKETHROUGH
-   :anchorlinks          Extensions/ANCHORLINKS
-   :all                  Extensions/ALL
-   :atxheaderspace       Extensions/ATXHEADERSPACE
-   :forcelistitempara    Extensions/FORCELISTITEMPARA
-   :relaxedhrules        Extensions/RELAXEDHRULES
-   :tasklistitems        Extensions/TASKLISTITEMS
-   :extanchorlinks       Extensions/EXTANCHORLINKS
-   :all-optionals        Extensions/ALL_OPTIONALS
-   :all-with-optionals   Extensions/ALL_WITH_OPTIONALS
-   :footnotes            Extensions/FOOTNOTES})
-
-(defn extensions-map->int [opts]
-  (->> opts
-       (merge {:autolinks true
-               :strikethrough true
-               :fenced-code-blocks true
-               :extanchorlinks true})
-       (filter val)
-       keys
-       (map extensions)
-       (apply bit-or 0)
-       int))
+;; Relevant links:
+;;;  Extensions: https://github.com/vsch/flexmark-java/wiki/Extensions
+;;;  Usage: https://github.com/vsch/flexmark-java/wiki/Usage
 
 (defn to-html [md-text]
-  (let [flexmark-opts (PegdownOptionsAdapter/flexmarkOptions
-                       (extensions-map->int extensions)
-                       (into-array Extension []))
-        builder (Parser/builder flexmark-opts)
-        builder (.set builder Parser/LISTS_ITEM_INDENT (int 2))
-        ;; builder (.set builder Parser/WWW_AUTO_LINK_ELEMENT true)
+  (let [extensions (Arrays/asList
+                    (into-array Object
+                                [; XXX: autolink has significant
+                                 ; performance impact on large files
+                                 (AutolinkExtension/create)
+                                 (StrikethroughExtension/create)
+                                 (TablesExtension/create)]))
+        options    (doto (MutableDataSet.) (.set Parser/EXTENSIONS extensions))
+        builder (Parser/builder options)
         parser (.build  builder)
-        renderer (.build (HtmlRenderer/builder flexmark-opts))]
+        renderer (.build (HtmlRenderer/builder options))]
     (->> md-text
          (.parse parser)
          (.render renderer))))
